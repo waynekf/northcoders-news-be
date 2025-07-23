e0549fbabb9baa2ee8cfbea7889e6e6d74babdd3
waynekf <misterdotnet@hotmail.com>
2025-07-23 11:07:44 +0100

    Apply prettier suggestions to codebase

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -100,7 +100,8 @@ describe("getArticles", () => {
         "mitch",
         "butter_bridge",
         "I find this existence challenging",
-        convertTimestampToDate({ created_at: "2020-07-09T21:11:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-07-09T21:11:00.000Z" })
+          .created_at,
         100,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -109,7 +110,8 @@ describe("getArticles", () => {
         "mitch",
         "icellusedkars",
         "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
-        convertTimestampToDate({ created_at: "2020-10-16T06:03:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-10-16T06:03:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -118,7 +120,8 @@ describe("getArticles", () => {
         "mitch",
         "icellusedkars",
         "some gifs",
-        convertTimestampToDate({ created_at: "2020-11-03T09:12:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-11-03T09:12:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -127,7 +130,8 @@ describe("getArticles", () => {
         "mitch",
         "rogersop",
         "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
-        convertTimestampToDate({ created_at: "2020-05-06T02:14:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-05-06T02:14:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -136,7 +140,8 @@ describe("getArticles", () => {
         "cats",
         "rogersop",
         "Bastet walks amongst us, and the cats are taking arms!",
-        convertTimestampToDate({ created_at: "2020-08-03T14:14:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-08-03T14:14:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -145,7 +150,8 @@ describe("getArticles", () => {
         "mitch",
         "icellusedkars",
         "Delicious tin of cat food",
-        convertTimestampToDate({ created_at: "2020-10-18T02:00:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-10-18T02:00:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -154,7 +160,8 @@ describe("getArticles", () => {
         "mitch",
         "icellusedkars",
         "I was hungry.",
-        convertTimestampToDate({ created_at: "2020-01-07T14:08:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-01-07T14:08:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -163,7 +170,8 @@ describe("getArticles", () => {
         "mitch",
         "icellusedkars",
         "Archaeologists have uncovered a gigantic statue from the dawn of humanity, and it has an uncanny resemblance to Mitch. Surely I am not the only person who can see this?!",
-        convertTimestampToDate({ created_at: "2020-04-17T02:08:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-04-17T02:08:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -172,7 +180,8 @@ describe("getArticles", () => {
         "mitch",
         "butter_bridge",
         "Well? Think about it.",
-        convertTimestampToDate({ created_at: "2020-06-06T10:10:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-06-06T10:10:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -181,7 +190,8 @@ describe("getArticles", () => {
         "mitch",
         "rogersop",
         "Who are we kidding, there is only one, and it's Mitch!",
-        convertTimestampToDate({ created_at: "2020-05-14T05:15:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-05-14T05:15:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -190,7 +200,8 @@ describe("getArticles", () => {
         "mitch",
         "icellusedkars",
         "Having run out of ideas for articles, I am staring at the wall blankly, like a cat. Does this make me a cat?",
-        convertTimestampToDate({ created_at: "2020-01-15T22:21:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-01-15T22:21:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -199,7 +210,8 @@ describe("getArticles", () => {
         "mitch",
         "butter_bridge",
         "Have you seen the size of that thing?",
-        convertTimestampToDate({ created_at: "2020-10-11T12:24:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-10-11T12:24:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -208,7 +220,8 @@ describe("getArticles", () => {
         "mitch",
         "butter_bridge",
         "There will never be enough articles about Mitch!",
-        convertTimestampToDate({ created_at: "2020-10-11T12:24:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-10-11T12:24:00.000Z" })
+          .created_at,
         0,
         "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
       ],
@@ -242,126 +255,144 @@ describe("getComments", () => {
         "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
         16,
         "butter_bridge",
-        convertTimestampToDate({ created_at: "2020-04-06T13:17:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-04-06T13:17:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
         14,
         "butter_bridge",
-        convertTimestampToDate({ created_at: "2020-10-31T03:03:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-10-31T03:03:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.",
         100,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-03-01T01:13:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-03-01T01:13:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         " I carry a log — yes. Is it funny to you? It is not to me.",
         -100,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-02-23T12:01:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-02-23T12:01:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "I hate streaming noses",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-11-03T21:00:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-11-03T21:00:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "I hate streaming eyes even more",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-04-11T22:02:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-04-11T22:02:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Lobster pot",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-05-15T21:19:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-05-15T21:19:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Delicious crackerbreads",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-04-14T21:19:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-04-14T21:19:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Superficially charming",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-01-01T03:08:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-01-01T03:08:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "git push origin master",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-06-20T08:24:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-06-20T08:24:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Ambidextrous marsupial",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-09-20T00:10:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-09-20T00:10:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Massive intercranial brain haemorrhage",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-03-02T07:10:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-03-02T07:10:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "Fruit pastilles",
         0,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-06-15T11:25:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-06-15T11:25:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.",
         16,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-06-09T06:00:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-06-09T06:00:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "I am 100% sure that we're not completely sure.",
         1,
         "butter_bridge",
-        convertTimestampToDate({ created_at: "2020-11-24T00:08:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-11-24T00:08:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "This is a bad article name",
         1,
         "butter_bridge",
-        convertTimestampToDate({ created_at: "2020-10-11T16:23:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-10-11T16:23:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "The owls are not what they seem.",
         20,
         "icellusedkars",
-        convertTimestampToDate({ created_at: "2020-03-14T17:02:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-03-14T17:02:00.000Z" })
+          .created_at,
       ],
       [
         undefined,
         "This morning, I showered for nine minutes.",
         16,
         "butter_bridge",
-        convertTimestampToDate({ created_at: "2020-07-21T01:20:00.000Z" }).created_at,
+        convertTimestampToDate({ created_at: "2020-07-21T01:20:00.000Z" })
+          .created_at,
       ],
     ];
 
--- queries.js
+++ queries.js
@@ -1,44 +1,43 @@
 const db = require("./db/connection.js");
 
 const users = () => {
-    db.query(`SELECT * FROM Users;`)
-        .then(({rows}) => {
-            console.log("all users:");
-            console.log(rows);
-        });
-    };
+  db.query(`SELECT * FROM Users;`).then(({ rows }) => {
+    console.log("all users:");
+    console.log(rows);
+  });
+};
 
 const codingArticles = () => {
-    db.query(`SELECT * FROM Articles WHERE Topic = 'coding';`)
-        .then(({rows}) => {
-            console.log("all coding articles:");
-            console.log(rows);
-        });
-    };
+  db.query(`SELECT * FROM Articles WHERE Topic = 'coding';`).then(
+    ({ rows }) => {
+      console.log("all coding articles:");
+      console.log(rows);
+    },
+  );
+};
 
 const commentsWithNegativeVotes = () => {
-    db.query(`SELECT * FROM Comments WHERE votes < 0;`)
-        .then(({rows}) => {
-            console.log("comments with negative votes:");
-            console.log(rows);
-        });
-    };
+  db.query(`SELECT * FROM Comments WHERE votes < 0;`).then(({ rows }) => {
+    console.log("comments with negative votes:");
+    console.log(rows);
+  });
+};
 
 const articlesByGrumpy19 = () => {
-    db.query(`SELECT * FROM Articles WHERE author = 'grumpy19';`)
-        .then(({rows}) => {
-            console.log("articles by Grumpy19:");
-            console.log(rows);
-        });
-    };
+  db.query(`SELECT * FROM Articles WHERE author = 'grumpy19';`).then(
+    ({ rows }) => {
+      console.log("articles by Grumpy19:");
+      console.log(rows);
+    },
+  );
+};
 
 const commentsWithMoreThan10Votes = () => {
-    db.query(`SELECT * FROM Comments WHERE votes > 10;`)
-        .then(({rows}) => {
-            console.log("comments with more than 10 votes:");
-            console.log(rows);
-        });
-    };
+  db.query(`SELECT * FROM Comments WHERE votes > 10;`).then(({ rows }) => {
+    console.log("comments with more than 10 votes:");
+    console.log(rows);
+  });
+};
 
 users();
 codingArticles();

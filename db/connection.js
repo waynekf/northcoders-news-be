33e94c41d6578a1c6fff508b9bc02b6d09d272ee
waynekf <misterdotnet@hotmail.com>
2025-07-29 16:34:14 +0100

    Implement hosting with Supabase and Render

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,23 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-module.exports = db;
+//module.exports = db;
+
+module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-const port = 9090;
+const { PORT = 9090 } = process.env;
 
-app.listen(port);
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));

7a55fcac95591b0bb0df454743f8aaa1feb57674
waynekf <misterdotnet@hotmail.com>
2025-08-13 09:51:26 +0100

    Fixed "not found" bug in fetchArticle API endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -55,6 +55,12 @@ const fetchArticle = function (article_id) {
     WHERE article_id = $1`;
 
   return db.query(sql, [article_id]).then(({ rows: articles }) => {
+    if (articles.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: `article '${article_id}' not found`,
+      });
+    }
     const article = articles[0];
     if (!article) {
       return Promise.reject({
